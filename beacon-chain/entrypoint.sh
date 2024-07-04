d707e6e584c4feffd7c1af6732778ad839be34b0
dappnodedev <diego@dappnode.io>
2024-07-04 15:59:00 +0200

    Create generic nimbus package

---
+++ beacon-chain/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+SUPPORTED_NETWORKS="gnosis holesky mainnet"
+MEVBOOST_FLAGS="--payload-builder=true --payload-builder-url"
+
+handle_checkpoint() {
+    # Run checkpoint sync script if provided
+    if [ -n "${CHECKPOINT_SYNC_URL}" ]; then
+        echo "[INFO - entrypoint] Running checkpoint sync script"
+
+        ${NIMBUS_BIN} trustedNodeSync \
+            --network="${NETWORK}" \
+            --trusted-node-url="${CHECKPOINT_SYNC_URL}" \
+            --backfill=false \
+            --data-dir="${DATA_DIR}"
+    else
+        echo "[WARN - entrypoint] No checkpoint sync script provided. Syncing from genesis."
+    fi
+}
+
+run_beacon() {
+    echo "[INFO - entrypoint] Running beacon node service"
+
+    # shellcheck disable=SC2086
+    exec ${NIMBUS_BIN} \
+        --network="${NETWORK}" \
+        --data-dir="${DATA_DIR}" \
+        --tcp-port="${P2P_PORT}" \
+        --udp-port="${P2P_PORT}" \
+        --log-level="${LOG_TYPE}" \
+        --rest \
+        --rest-port=3500 \
+        --rest-address=0.0.0.0 \
+        --metrics \
+        --metrics-address=0.0.0.0 \
+        --metrics-port=8008 \
+        --jwt-secret=/jwtsecret \
+        --web3-url="${ENGINE_API_URL}" \
+        --suggested-fee-recipient="${FEE_RECIPIENT}" ${EXTRA_OPTS}
+}
+
+format_graffiti
+set_beacon_config_by_network "${SUPPORTED_NETWORKS}"
+handle_checkpoint
+set_mevboost_flag "${MEVBOOST_FLAGS}" # MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
+run_beacon
---
+++ validator/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+SUPPORTED_NETWORKS="gnosis holesky mainnet"
+MEVBOOST_FLAG="--payload-builder=true"
+SKIP_MEVBOOST_URL="true"
+CLIENT="nimbus"
+
+# shellcheck disable=SC1091
+. /etc/profile
+
+run_validator() {
+    echo "[INFO - entrypoint] Running validator service"
+
+    # shellcheck disable=SC2086
+    exec nimbus_validator_client \
+        --log-level="${LOG_TYPE}" \
+        --doppelganger-detection="${ENABLE_DOPPELGANGER}" \
+        --non-interactive=true \
+        --web3-signer-url="${WEB3SIGNER_API_URL}" \
+        --suggested-fee-recipient="${FEE_RECIPIENT}" \
+        --keymanager=true \
+        --keymanager-port=3500 \
+        --keymanager-address=0.0.0.0 \
+        --keymanager-allow-origin=* \
+        --keymanager-token-file="${AUTH_TOKEN_PATH}" \
+        --metrics=true \
+        --metrics-address=0.0.0.0 \
+        --metrics-port=8008 \
+        --graffiti="${GRAFFITI}" \
+        --beacon-node="${BEACON_NODE_API_URL}" ${EXTRA_OPTS}
+}
+
+format_graffiti
+set_validator_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}"
+set_mevboost_flag "${MEVBOOST_FLAG}" "${SKIP_MEVBOOST_URL}" # MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
+run_validator
