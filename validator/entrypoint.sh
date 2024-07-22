5e868dacc6086abeb2a8d34f58a2facdbd30a92a
dappnodedev <diego@dappnode.io>
2024-07-22 12:25:11 +0200

    Use latest staker package scripts version

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -8,29 +8,29 @@ CLIENT="nimbus"
 # shellcheck disable=SC1091
 . /etc/profile
 
-run_validator() {
-    echo "[INFO - entrypoint] Running validator service"
+VALID_GRAFFITI=$(get_valid_graffiti "${GRAFFITI}")
+VALID_FEE_RECIPIENT=$(get_valid_fee_recipient "${FEE_RECIPIENT}")
+SIGNER_API_URL=$(get_signer_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}")
+BEACON_API_URL=$(get_beacon_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}")
+MEVBOOST_FLAG=$(get_mevboost_flag "${MEVBOOST_FLAG_KEY}" "${SKIP_MEVBOOST_URL}")
 
-    # shellcheck disable=SC2086
-    exec ${NIMBUS_BIN} \
-        --log-level="${LOG_TYPE}" \
-        --doppelganger-detection="${ENABLE_DOPPELGANGER}" \
-        --non-interactive=true \
-        --web3-signer-url="${WEB3SIGNER_API_URL}" \
-        --suggested-fee-recipient="${FEE_RECIPIENT}" \
-        --keymanager=true \
-        --keymanager-port=3500 \
-        --keymanager-address=0.0.0.0 \
-        --keymanager-allow-origin=* \
-        --keymanager-token-file="${VALIDATOR_API_TOKEN_PATH}" \
-        --metrics=true \
-        --metrics-address=0.0.0.0 \
-        --metrics-port=8008 \
-        --graffiti="${GRAFFITI}" \
-        --beacon-node="${BEACON_API_URL}" ${EXTRA_OPTS}
-}
+echo "[INFO - entrypoint] Running validator service"
 
-format_graffiti
-set_validator_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}"
-set_mevboost_flag "${MEVBOOST_FLAG}" "${SKIP_MEVBOOST_URL}" # MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
-run_validator
+# shellcheck disable=SC2086
+exec ${NIMBUS_BIN} \
+    --log-level="${LOG_TYPE}" \
+    --data-dir="${DATA_DIR}" \
+    --doppelganger-detection="${ENABLE_DOPPELGANGER}" \
+    --non-interactive \
+    --web3-signer-url="${SIGNER_API_URL}" \
+    --suggested-fee-recipient="${VALID_FEE_RECIPIENT}" \
+    --keymanager=true \
+    --keymanager-port=3500 \
+    --keymanager-address=0.0.0.0 \
+    --keymanager-allow-origin=* \
+    --keymanager-token-file="${VALIDATOR_API_TOKEN_PATH}" \
+    --metrics=true \
+    --metrics-address=0.0.0.0 \
+    --metrics-port=8008 \
+    --graffiti="${VALID_GRAFFITI}" \
+    --beacon-node="${BEACON_API_URL}" ${MEVBOOST_FLAG} ${EXTRA_OPTS}
