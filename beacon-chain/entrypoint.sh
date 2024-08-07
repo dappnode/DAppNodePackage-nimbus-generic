9b6dd5256940945ded4edcb85900547e3baac482
dappnodedev <144998261+dappnodedev@users.noreply.github.com>
2024-08-07 16:49:10 +0200

    Add beacon-validator proxy (#1)
    * Add beacon-validator proxy
    
    * Set boolean as string
    
    * Quiet on data dir not found
    
    * Improve package user permission
    
    * Use latest staker package scripts version

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -1,55 +1,46 @@
 #!/bin/sh
 
 SUPPORTED_NETWORKS="gnosis holesky mainnet"
-MEVBOOST_FLAGS="--payload-builder=true --payload-builder-url"
+MEVBOOST_FLAG_KEYS="--payload-builder=true --payload-builder-url"
 
 # shellcheck disable=SC1091 # Path is relative to the Dockerfile
 . /etc/profile
 
-handle_checkpoint() {
+ENGINE_URL=$(get_engine_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}")
+VALID_FEE_RECIPIENT=$(get_valid_fee_recipient "${FEE_RECIPIENT}")
+MEVBOOST_FLAG=$(get_mevboost_flag "${NETWORK}" "${MEVBOOST_FLAG_KEYS}")
 
-    if [ -n "$(ls -A "${DATA_DIR}/db")" ]; then
-        echo "[INFO - entrypoint] Data directory has already been initialized, skipping checkpoint sync."
-        return
-    fi
+if [ -n "$(ls -A "${DATA_DIR}/db" 2>/dev/null)" ]; then
+    echo "[INFO - entrypoint] Data directory has already been initialized, skipping checkpoint sync."
 
-    # Run checkpoint sync script if provided
-    if [ -n "${CHECKPOINT_SYNC_URL}" ]; then
-        echo "[INFO - entrypoint] Running checkpoint sync script"
+elif [ -n "${CHECKPOINT_SYNC_URL}" ]; then
+    echo "[INFO - entrypoint] Running checkpoint sync script"
 
-        ${NIMBUS_BIN} trustedNodeSync \
-            --network="${NETWORK}" \
-            --trusted-node-url="${CHECKPOINT_SYNC_URL}" \
-            --backfill=false \
-            --data-dir="${DATA_DIR}"
-    else
-        echo "[WARN - entrypoint] No checkpoint sync script provided. Syncing from genesis."
-    fi
-}
-
-run_beacon() {
-    echo "[INFO - entrypoint] Running beacon node service"
-
-    # shellcheck disable=SC2086
-    exec ${NIMBUS_BIN} \
+    ${NIMBUS_BIN} trustedNodeSync \
         --network="${NETWORK}" \
-        --data-dir="${DATA_DIR}" \
-        --tcp-port="${P2P_PORT}" \
-        --udp-port="${P2P_PORT}" \
-        --log-level="${LOG_TYPE}" \
-        --rest \
-        --rest-port=3500 \
-        --rest-address=0.0.0.0 \
-        --metrics \
-        --metrics-address=0.0.0.0 \
-        --metrics-port=8008 \
-        --jwt-secret=/jwtsecret \
-        --web3-url="${ENGINE_API_URL}" \
-        --suggested-fee-recipient="${FEE_RECIPIENT}" ${EXTRA_OPTS}
-}
+        --trusted-node-url="${CHECKPOINT_SYNC_URL}" \
+        --backfill=false \
+        --data-dir="${DATA_DIR}"
 
-format_graffiti
-set_beacon_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}"
-handle_checkpoint
-set_mevboost_flag "${MEVBOOST_FLAGS}" # MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
-run_beacon
+else
+    echo "[WARN - entrypoint] No checkpoint sync script provided. Syncing from genesis."
+fi
+
+echo "[INFO - entrypoint] Running beacon node service"
+
+# shellcheck disable=SC2086
+exec ${NIMBUS_BIN} \
+    --network="${NETWORK}" \
+    --data-dir="${DATA_DIR}" \
+    --tcp-port="${P2P_PORT}" \
+    --udp-port="${P2P_PORT}" \
+    --log-level="${LOG_TYPE}" \
+    --rest \
+    --rest-port=3500 \
+    --rest-address=0.0.0.0 \
+    --metrics \
+    --metrics-address=0.0.0.0 \
+    --metrics-port=8008 \
+    --jwt-secret=/jwtsecret \
+    --web3-url="${ENGINE_URL}" \
+    --suggested-fee-recipient="${VALID_FEE_RECIPIENT}" ${MEVBOOST_FLAG} ${EXTRA_OPTS}
